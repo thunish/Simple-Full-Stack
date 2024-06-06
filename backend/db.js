e6ba227a73d39b7260a804f1fc41027adb63698b
Thunish Chowdary Kondabolu <154307741+thunish@users.noreply.github.com>
2024-06-06 22:02:44 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://Thunish:ZKxPh0wHJ5NB6hAX@cluster0.bvmakoh.mongodb.net/paytm")
+mongoose.connect("Place you own mongodb instance")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
